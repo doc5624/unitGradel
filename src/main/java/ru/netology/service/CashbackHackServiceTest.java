2717f15ecc8df54df6e9982d61f3c0efd7a7a472
doc5624 <doc5624@yandex.ru>
2024-08-01 00:12:26 +0700

    junit4 commit

--- src/main/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/main/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,18 +1,29 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
-import org.testng.Assert;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class CashbackHackServiceTest {
 
     @Test
-    public void remainTest() {
+   public void remainTest() {
 
         CashbackHackService cashService = new CashbackHackService();
 
         int expected = 200;
         int actual = cashService.remain(800);
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void remainTestBag() {
+
+        CashbackHackService cashService = new CashbackHackService();
+
+        int expected = 0;
+        int actual = cashService.remain(1000);
+        Assert.assertEquals(expected, actual);
 
     }
 
